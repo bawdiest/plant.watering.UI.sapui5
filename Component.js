3137bd63beb8f584ee44f157ca78f263c8bdcd8b
bawdiest <mikmak@mikmaks-mbp.home>
2016-08-20 01:34:56 +0200

    First Commit
    First Commit

---
+++ Component.js
@@ -0,0 +1,30 @@
+sap.ui.define([
+   "sap/ui/core/UIComponent",
+   "sap/ui/model/json/JSONModel",
+   "sap/ui/model/resource/ResourceModel"
+], function (UIComponent, JSONModel, ResourceModel) {
+   "use strict";
+   return UIComponent.extend("sap.ui.demo.wt.Component", {
+            metadata : {
+               manifest: "json"
+	},
+      init : function () {
+         // call the init function of the parent
+         UIComponent.prototype.init.apply(this, arguments);
+         // set data model
+         var oData = {
+            input : {
+               result : "World"
+            }
+         };
+         var oModel = new JSONModel(oData);
+         this.setModel(oModel);
+
+         // set i18n model
+         var i18nModel = new ResourceModel({
+            bundleName : "sap.ui.demo.wt.i18n.i18n"
+         });
+         this.setModel(i18nModel, "i18n");
+      }
+   });
+});
