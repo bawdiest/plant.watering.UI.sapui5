c24c511f5036863f8a73c02151dcc84cb349371b
bawdiest <mikmak@mikmaks-mbp.home>
2016-08-26 00:53:42 +0200

    modularising view controller
    use one predict function, when different events are fired

--- controller/InputOutputPanel.controller.js
+++ controller/InputOutputPanel.controller.js
@@ -1,30 +1,39 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
-   "use strict";
-   return Controller.extend("sap.ui.demo.wt.controller.InputOutputPanel", {
-      onPredict : function () {
+   ], function (Controller, MessageToast) {
+      "use strict";
+      return Controller.extend("sap.ui.demo.wt.controller.InputOutputPanel", {
+         onAfterRendering : function () {
+            var that = this;
+            that.predict();
+            //jQuery.sap.delayedCall(0, that, function() {
+            //   that.predict()
+            //})
+            },
+            onPredict : function () {
+               this.predict()
+            },
+            predict : function () {
+               var that = this;
          // read msg from i18n model
-         var oBundle = this.getView().getModel("i18n").getResourceBundle();
-         var sTemperature = this.getView().getModel("test").getProperty("/foo");
+         var oBundle = that.getView().getModel("i18n").getResourceBundle();
+         var sTemperature = that.getView().getModel("test").getProperty("/foo");
          var sResult = sTemperature;
          var sMsg = oBundle.getText("resultMsg", [sResult]);
-         
+         var dMsg = oBundle.getText("doneMsg");
 
-var resultPanel = this.getView().byId("Result");
+         var resultPanel = that.getView().byId("Result");
          resultPanel.setBusy(true);
 
          //this.getView().getModel().setProperty("/output/result", "Calculating...");
 
 // simulate delayed end of operation
-         jQuery.sap.delayedCall(1000, this, function () {
-            resultPanel.setBusy(false)
-            this.getView().getModel().setProperty("/result/message", sMsg);
-            MessageToast.show(sMsg);
-         });
-         // show message
-         MessageToast.show(sMsg);
-      }
-   });
+jQuery.sap.delayedCall(1000, that, function () {
+   resultPanel.setBusy(false)
+   that.getView().getModel().setProperty("/result/message", sMsg);
+   MessageToast.show(dMsg);
 });
+}
+});
+   });
