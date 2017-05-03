a231d362c859f9c3bf5a23955d01560d8cbca153
bawdiest <com@mikmak.cc>
2017-05-04 00:35:48 +0200

    Redefine Launchpad

--- Component.js
+++ Component.js
@@ -1,90 +1,58 @@
-   sap.ui.define([
-      "sap/ui/core/UIComponent",
-      "sap/ui/model/json/JSONModel",
-      "sap/ui/model/resource/ResourceModel"
-      ], function (UIComponent, JSONModel, ResourceModel) {
-         "use strict";
-         return UIComponent.extend("sap.ui.demo.wt.Component", {
-            metadata : {
-               manifest: "json"
-            },
-            init : function () {
+sap.ui.define([
+   "sap/ui/core/UIComponent",
+   "sap/ui/model/json/JSONModel",
+   "sap/ui/model/resource/ResourceModel"
+   ], function (UIComponent, JSONModel, ResourceModel) {
+      "use strict";
+      return UIComponent.extend("sap.ui.demo.wt.Component", {
+         metadata : {
+            manifest: "json"
+         },
+         init : function () {
 
-         // call the init function of the parent
-         UIComponent.prototype.init.apply(this, arguments);
+      // call the init function of the parent
+      UIComponent.prototype.init.apply(this, arguments);
 
 // create the views based on the url/hash
 this.getRouter().initialize();
 
-         // set data model
-         var oData = {
-            input : {
-               temperature : 25,
-               irrigationTime: 0
-            },
-            result : {
-               message : "",
-               time : 0,
-               startIrrigationLinkText: "",
-               quantitiy : 0
-            }
-         };
-         var oModel = new JSONModel(oData);
-         this.setModel(oModel);
 
-         var weatherData = {
-            weather : {
-               temperature : 25,
-               pressure: 960,
-               wind: 5,
-               rain: 0,
-               humidity: 85
-            }
-         };
-         var weatherModel = new JSONModel(weatherData);
-         this.setModel(weatherModel, "wd");
+var url = "https://cors-anywhere.herokuapp.com/http://mikmak.cc:1880/weatherData?temperature=21";
+url += "&pressure=966";
+url += "&wind=1";
+url += "&rain=0";
+url += "&humidity=85";
+var parameters = {};
+var headers ={};
+var testModel = new JSONModel();
+      testModel.loadData(url, parameters, false, "GET", false, false, headers); //false: wait until data is loaded
+      this.setModel(testModel, "currentWeather");
+
+      var oModel = new sap.ui.model.json.JSONModel();
+      oModel.loadData("data/predictiveModel.json");
+
+      this.setModel(oModel, "model");
 
 
-         var url = "https://cors-anywhere.herokuapp.com/http://mikmak.cc:1880/weatherData?temperature=21";
-         url += "&pressure=966";
-         url += "&wind=1";
-         url += "&rain=0";
-         url += "&humidity=85";
-         var parameters = {};
-         var headers ={};
-         var testModel = new JSONModel();
-         testModel.loadData(url, parameters, false, "GET", false, false, headers); //false: wait until data is loaded
-         this.setModel(testModel, "currentWeather");
-
-         // var url = "https://cors-anywhere.herokuapp.com/http://mikmak.cc:1880/weatherData?foo=21"; //https://cors-anywhere.herokuapp.com/
-         // var parameters = {};
-         // var headers ={};
-         // var testModel = new JSONModel();
-         // testModel.loadData(url, parameters, true, "GET", false, false, headers); //false: wait until data is loaded
-         // this.setModel(testModel, "currentWeather";
-
-         var oModel = new sap.ui.model.json.JSONModel();
-         oModel.loadData("data/predictiveModel.json");
-
-         this.setModel(oModel, "model");
-
-
-         // set i18n model
-         var i18nModel = new ResourceModel({
-            bundleName : "sap.ui.demo.wt.i18n.i18n"
-         });
-         this.setModel(i18nModel, "i18n");
-
-
-         var oData = {
-            data : {
-               irrigationTime: 0,
-               irrigationPerc: 0,
-               pumpStoped: 0
-            }
-         };
-         var oModel = new JSONModel(oData);
-         this.setModel(oModel, "irrigationProcess");
-      }
-   });
+      // set i18n model
+      var i18nModel = new ResourceModel({
+         bundleName : "sap.ui.demo.wt.i18n.i18n"
       });
+      this.setModel(i18nModel, "i18n");
+
+            var sPath = jQuery.sap.getModulePath("sap.ui.demo.wt", "/data/homeData.json");
+            var oModel = new JSONModel(sPath);
+            this.setModel(oModel, "tiles");
+
+      var oData = {
+         data : {
+            irrigationTime: 0,
+            irrigationPerc: 0,
+            pumpStoped: 0
+         }
+      };
+      var oModel = new JSONModel(oData);
+      this.setModel(oModel, "irrigationProcess");
+   }
+});
+   });
--- controller/Home.controller.js
+++ controller/Home.controller.js
@@ -1,45 +1,65 @@
     sap.ui.define([
-    		'jquery.sap.global',
-    		'sap/ui/core/mvc/Controller',
-            'sap/m/MessageToast',
-    		'sap/ui/model/json/JSONModel'
-    	], function(jQuery, Controller, MessageToast, JSONModel) {
-    	"use strict";
-     
-    	var PageController = Controller.extend("sap.ui.demo.wt.controller.Home", {
-     
-    		onInit : function (evt) {
+      'jquery.sap.global',
+      'sap/ui/core/mvc/Controller',
+      'sap/m/MessageToast',
+      'sap/ui/model/json/JSONModel'
+      ], function(jQuery, Controller, MessageToast, JSONModel) {
+         "use strict";
+
+         var PageController = Controller.extend("sap.ui.demo.wt.controller.Home", {
+
+            // Init is done in Componet.js
+         /* onInit : function (evt) {
     			// set mock model
     			var sPath = jQuery.sap.getModulePath("sap.ui.demo.wt", "/data/homeData.json");
     			var oModel = new JSONModel(sPath);
-    			this.getView().setModel(oModel);
-    		},
-     
-    		handleEditPress : function (evt) {
-    			var oTileContainer = this.getView().byId("container");
-    			var newValue = ! oTileContainer.getEditable();
-    			oTileContainer.setEditable(newValue);
-    			evt.getSource().setText(newValue ? "Done" : "Edit");
-    		},
-     
-    		handleBusyPress : function (evt) {
-    			var oTileContainer = this.getView().byId("container");
-    			var newValue = ! oTileContainer.getBusy();
-    			oTileContainer.setBusy(newValue);
-    			evt.getSource().setText(newValue ? "Done" : "Busy state");
-    		},
-     
-    		handleTileDelete : function (evt) {
-    			var tile = evt.getParameter("tile");
-    			evt.getSource().removeTile(tile);
-    		},
+    			this.getView().setModel(oModel, "tiles");
+
+                //var agr = this.getView().getModel("tiles").setProperty("/TileCollection/0/number", 17);
+            }, */
+
+            onAfterRendering : function () {
+                var that = this;
+                that.predict();
+            },
+
+
 
             handleTilePressed : function(evt) {
                 var oTile = evt.getSource();
-                MessageToast.show("The GenericTile \"" + oTile.getTitle() + "\" was pressed.");
+                MessageToast.show("The GenericTile \"" + oTile.getHeader() + "\" was pressed.");
+            },
+
+            predict : function () {
+                var that = this;
+                var url = "http://mikmak.cc:1880/predictIrrigation";
+                that.getView().getModel().setProperty("/result/state", "Loading");
+
+                $
+                .ajax({
+                    url : url,
+                    async: true,
+                    jsonpCallback: 'processJSON', 
+                    contentType : "application/json",
+                    dataType : 'jsonp',
+                    success: function (result) {  
+                        var sQuantity = result.Quantity;
+                        var sTime = result.Time;
+
+                        var dMsg = oBundle.getText("doneMsg");
+                        that.getView().getModel().setProperty("/result/state", "Loaded");
+
+                        that.getView().getModel().setProperty("/result/time", sTime);
+                        that.getView().getModel().setProperty("/result/quantity", sQuantity);
+                    }, 
+                    error: function (e) {  
+                        alert("Not OK")
+                        console.log(e.message);  
+                    }  
+                })
             }
-    	});
-     
-    	return PageController;
-     
-    });
+
+
+        })
+         return PageController;
+     }) 
