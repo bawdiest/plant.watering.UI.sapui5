a88ad1b54cc089dbf20e2a727273b5103e4d6a3a
airibot <airigare@mikmak.cc>
2018-03-07 23:16:54 +0100

    App developped in SAPWEBIDE

---
+++ dist/controller/Weather.controller.js
@@ -0,0 +1,34 @@
+sap.ui.define([
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/mvc/Controller"
+], function(JSONModel, Controller) {
+	"use strict";
+
+	return Controller.extend("airigareairigareWeather.controller.Weather", {
+
+		onInit: function() {
+			var that = this;
+
+			var url = "/weatherDW/";
+			$
+				.ajax({
+					url: url,
+					async: false,
+					//jsonpCallback: 'processJSON', 
+//					contentType: "application/json",
+//					dataType: 'jsonp',
+					success: function(result) {
+						var oModel = new sap.ui.model.json.JSONModel(result);
+						that.getView().setModel(oModel, "weatherData");
+					},
+					error: function(jqXHR, textStatus, errorThrown) {
+						//MessageToast.show("Auftrag konnte nicht angelegt werden");
+					},
+					complete: function(jqHXR, textStatus) {
+						//MessageToast.show("Auftrag angelegt");
+						
+					}
+				});
+		}
+	});
+});
