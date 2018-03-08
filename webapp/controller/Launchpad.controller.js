3fdc4ac432d06ce22f6ba7b3c36155c004dd8a1f
Mikhail Makarov <mikhail.makarov@swisscom.com>
2018-03-08 19:23:43 +0000

    Enable Navigation

---
+++ webapp/controller/Launchpad.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/mvc/Controller"
+], function(JSONModel, Controller) {
+	"use strict";
+
+	return Controller.extend("airigareairigareWeather.controller.Launchpad", {
+
+		onInit: function() {
+			var that = this;
+
+		}
+	});
+});
